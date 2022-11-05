681525ef97f6f2f449ca55532e2d895b95c480cd
JD Rowley <jdrowley1989@gmail.com>
2022-11-05 08:56:02 -0600

    updated program.cs and appsettings.json files

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,5 +4,12 @@ namespace InspectionAPI.Data
 {
 	public class DataContext : DbContext
 	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		public DbSet<Inspection> Inspections { get; set; }
+
+		public DbSet<Type> Types { get; set; }
+
+		public DbSet<Status> Statuses { get; set; }
 	}
 }
